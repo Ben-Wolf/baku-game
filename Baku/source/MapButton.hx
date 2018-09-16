1c1842e49761436c0fecf1450049057d83efaa54
Ehren <omegathesigma@gmail.com>
2018-09-16 12:02:07 -0400

    Added multiple hitbox support

--- Baku/source/MapButton.hx
+++ Baku/source/MapButton.hx
@@ -2,39 +2,39 @@ package;
 
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.group.FlxGroup;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.input.mouse.FlxMouseEventManager;
 
 class MapButton extends FlxState {
-	var _levelbutton:FlxSprite;
+	var _levelbutton:FlxTypedGroup<FlxSprite>;
 	var _leveloutline:FlxSprite;
 	var _loadTarget:String;
 	public function new(x:Int, y:Int, color:FlxColor, loadTarget:String){
 		super();
+		_levelbutton = new FlxTypedGroup<FlxSprite>(5);
 		_loadTarget = loadTarget;
-		
-		_levelbutton = new FlxSprite(x, y);
-		_levelbutton.makeGraphic(32, 32, color);
-		_levelbutton.alpha = 0;
-		add(_levelbutton);
 		
 		_leveloutline = new FlxSprite(x, y);
 		_leveloutline.makeGraphic(50, 50, color);
 		add(_leveloutline);
+		add(_levelbutton);
 		_leveloutline.alpha = 0;
-		
-		FlxMouseEventManager.add(_levelbutton, clicked, null , hover, out);
 	}
 	
-	public function setRelativeHitbox(x:Int, y:Int, width:Int, height:Int):Void{
-		_levelbutton.x = _levelbutton.x + x;
-		_levelbutton.y = _levelbutton.y + y;
-		_levelbutton.setGraphicSize(width, height);
+	public function addRelativeHitbox(x:Int, y:Int, width:Int, height:Int):Void{
+		var levelbutton = new FlxSprite(_leveloutline.x + x, _leveloutline.y + y);
+		levelbutton.makeGraphic(width, height, 0xFFaa1111);
+		levelbutton.alpha = 0;
+		_levelbutton.add(levelbutton);
+		FlxMouseEventManager.add(levelbutton, clicked, null , hover, out);
 	}
 	
 	public function showHitbox():Void{
-		_levelbutton.alpha = 1;
+		for (b in _levelbutton){
+			b.alpha = 1;
+		}
 	}
 	
 	override public function create():Void {
