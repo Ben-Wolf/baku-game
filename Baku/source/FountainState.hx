a505e02e117dd8b615dffe7baa521593c0d76eb6
Ehren <omegathesigma@gmail.com>
2018-09-20 18:41:44 -0400

    Added more buttons, revised dream state button mechanics

--- Baku/source/FountainState.hx
+++ Baku/source/FountainState.hx
@@ -10,16 +10,22 @@ class FountainState extends FlxState {
 	var _mapButton:MapButton;
 	var _exchangeButton:FlxSprite;
 	var _exitbutton:ExitButton;
+	var _backdrop:FlxSprite;
 	override public function create():Void {
 		super.create();
 		bgColor = 0xFFAA6e99;
+		
+		_backdrop = new FlxSprite(0, 0);
+		_backdrop.loadGraphic("assets/images/baku_statue.png", true, 680, 453);
+		_backdrop.animation.add("animate", [0, 1, 2], 6, false);
+		add(_backdrop);
 		
 		var _shards = new ShardsUI();
 		add(_shards);
 		
 		
-		_mapButton = new MapButton(100, 100, "assets/images/level1outline.png", "MapState");
-		_mapButton.addRelativeHitbox(9, 9, 32, 32);
+		_mapButton = new MapButton(100, 100, "assets/images/backtomapoutline.png", "MapState");
+		_mapButton.addRelativeHitbox(1, 1, 100, 44);
 		_mapButton.showHitbox();
 		add(_mapButton);
 		
@@ -36,6 +42,7 @@ class FountainState extends FlxState {
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+		_backdrop.animation.play("animate");
 	}
 	
 	public function hover(s:FlxSprite){
