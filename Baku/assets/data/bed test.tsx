3b2c4d4d95613e6d33ceb2fb59793ca78defa494
Unknown <mastrl2@rpi.edu>
2018-09-21 13:44:24 -0400

    Bed test is here now

--- Baku/assets/data/bed test.tsx
+++ Baku/assets/data/bed test.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="bed test" tilewidth="32" tileheight="32" tilecount="36" columns="6">
- <image source="Bed.png" width="196" height="196"/>
+ <image source="../../../Baku/baku-game/Baku/assets/images/Bed.png" width="196" height="196"/>
 </tileset>
