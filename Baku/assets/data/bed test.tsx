a18f61964a8d63a78b367f406c18282c6f07a228
Unknown <mastrl2@rpi.edu>
2018-09-21 13:28:32 -0400

    Fixed?

---
+++ Baku/assets/data/bed test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bed test" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="Bed.png" width="196" height="196"/>
+</tileset>
